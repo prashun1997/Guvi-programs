b15abb74443a68b541a26dddb7e7007dcd2a30e0
Prashun Dalmia <prashun1997@gmail.com>
2018-01-13 23:59:37 +0530

    Program for reversal of string #1 program in guvi

---
+++ player/reverse.java
@@ -0,0 +1,14 @@
+import java.util.*;
+import java.io.*;
+class Reverse
+{
+	public static void main(String args[])
+	{
+		Scanner sc=new Scanner(System.in);
+		String s=sc.next();
+		int l=s.length();
+		for (int i=l-1;i>=0;i--)
+			System.out.print(s.charAt(i));
+
+	}
+}
